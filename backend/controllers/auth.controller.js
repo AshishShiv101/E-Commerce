c25739386635d86704f376df735a5f44c754d097
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2024-12-17 13:07:23 +0530

    Cache established

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import Redis from "ioredis";
 import User from "../models/user.models.js"
 import jwt from "jsonwebtoken"
 
@@ -39,19 +40,44 @@ export const signup = async (req, res) => {
 
         setCookies(res,accessToken,refreshToken)
 
-        res.status(201).json({ user:{
+        res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
             role: user.role,
-        }, message: "User created" });
+    });
+
     } catch (error) {
         res.status(500).json({ message:error.message});
     }
 }
 
 export const login = async (req, res) => {
-    res.send('login');
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({ email });
+
+        if(user && (await user.completePassword(password))) {
+            const {accessToken, refreshToken} = await storeRefreshToken(user._id)
+
+            await storeRefreshToken(user._id, refreshToken)
+            setCookies(res, accessToken, refreshToken)
+
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                message: "Logged in successfully"
+            })
+        } else{
+            res.status(401).json({message: "Authentication failed"})
+        }
+
+    } catch (error) {
+        console.log("Error in login controller",error.message)
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export const logout = async (req, res) => {
@@ -59,13 +85,44 @@ export const logout = async (req, res) => {
         const refreshToken = req.cookies.refresh-token;
         if(refreshToken){
             const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET)
-            await redis.del(`refresh_token:${decoded.userId}`)
+            await Redis.del(`refresh_token:${decoded.userId}`)
         }
 
         res.clearCookie('access-token');
         res.clearCookie('refresh-token');
         res.json({ message: "Logged out" });
     } catch (error) {
+        console.log("Error in logout controller",error.message)
         res.status(500).json({message:"Server Error",error:error.message})
     }
 }
+
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refresh-token;
+
+        if(!refreshToken){
+            return res.status(403).json({message: "Refresh token is missing"})
+        }
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+        const storedToken = await redis.get('refresh_token:${decoded.userId}');
+        if(!storedToken || storedToken!== refreshToken){
+            return res.status(403).json({message: "Refresh token is invalid"})
+        }
+
+        const accessToken = jwt.sign({userId: decoded.userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"15m"})
+        
+        res.cookie("accessToken",accessToken,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite:"strict",
+            maxAge: 15* 60 * 1000,
+        })
+
+        res.json({message:"token refreshed successfully"})
+    } catch (error) {
+        console.log("Error in refreshToken controller",error.message)
+        res.status(500).json({message:"Server Error",error:error.message})
+    }
+}
+
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup,refreshToken } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post('/login', login)
 
 router.post('/logout', logout)
 
+router.post('/refresh-token', refreshToken)
+
+router.post("/profile",getProfile)
 export default router
 
