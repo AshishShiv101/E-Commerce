b81272f091ebf085b8c857b03785672f771e0528
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2024-12-25 15:20:44 +0530

    Payment methodsstarted

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,22 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.routes.js "
+import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;  // Using the PORT from .env file, default to 5000 if not defined
 
 app.use(express.json());  // Middleware to parse incoming JSON requests
-app.use(cookieParser()); // Middleware to)
-// Mount the authentication routes
+app.use(cookieParser()); 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons",couponRoutes)
+app.unsubscribe("api/payments",paymentRoutes)
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     connectDB();  // Connect to the database when the server starts
